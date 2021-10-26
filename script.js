6eadf09688897e8db7c29683ef616d11b62da562
Pavan-rmes <62424109+Pavan-rmes@users.noreply.github.com>
2021-10-26 18:04:11 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+async function getCoin(){
+    try{
+       let getCoinList = await fetch(`https://api.jikan.moe/v3/search/anime?q=${AnimeName}`)
+       let data = await getCoinList.json()
+       data = data.results
+       return data
+    }
+    catch(err){
+        console.log(err)
+    }
+}
+
 
 
 const ctx = document.getElementById('myChart').getContext('2d');
