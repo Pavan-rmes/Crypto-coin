8f7d4f165dccbb01cb507817a34038bd49dec82c
Pavan-rmes <62424109+Pavan-rmes@users.noreply.github.com>
2021-10-29 08:11:01 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,33 +1,88 @@
-async function getCoin(){
+async function getAllCoin(){
     try{
-       let getCoinList = await fetch(`https://api.jikan.moe/v3/search/anime?q=${AnimeName}`)
-       let data = await getCoinList.json()
-       data = data.results
-       return data
+       let getCoinList = await fetch(`https://api.coincap.io/v2/assets`)
+       let allCoins = await getCoinList.json()
+        allCoins = allCoins.data
+       return allCoins;
     }
     catch(err){
-        console.log(err)
+        var body = document.querySelector('body')
+        body.innerHTML=""
+        body.innerHTML =err
     }
 }
 
 
 
-const ctx = document.getElementById('myChart').getContext('2d');
-const myChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [1,2,3,4,5]
-        }]
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
+getAllCoin()
+.then((data)=>{
+    console.log(data)
+    for(let i=0;i<20;i++)
+    {
+    var coinNames = document.querySelector('ul.coin-hidden')
+    let list = document.createElement('li')
+    roundOff = Math.round(parseFloat(data[i].priceUsd) *100)/100
+    list.innerHTML=`<a href="#">${data[i].symbol}</a> <span class="crypto-value">${roundOff}</span>`
+    coinNames.appendChild(list)
+    coinNames.appendChild(document.createElement('hr'))
+    }
+})
+.catch((err)=>{
+    var body = document.querySelector('body')
+    body.innerHTML=""
+    body.innerHTML ="<h1>Not Found</h1>"
+
+})
+
+
+
+
+async function getPerticularCoin()
+{
+    try {
+        let coinData = await fetch(`https://api.coincap.io/v2/assets/bitcoin/history?interval=d1`)
+        let singleCoin = await coinData.json()
+        return singleCoin.data
+        
+    } catch (error) {
+        var body = document.querySelector('body')
+        body.innerHTML=""
+        body.innerHTML ="<h1>Not Found</h1>"
+    }
+}
+
+getPerticularCoin()
+.then((data)=>{
+    console.log(data)
+    let coinDate =[]
+    let coinValue=[]
+
+    for(i in data)
+    {
+        let everyDate =data[i].date
+        everyDate =  everyDate.split("T")[0]
+        coinDate.push(everyDate)
+        coinValue.push(data[i].priceUsd)
+    }
+    const ctx = document.getElementById('myChart').getContext('2d');
+    const myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: coinDate,
+            datasets: [{
+                label: '# of Votes',
+                data: coinValue
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
             }
         }
-    }
-});
+    });
+
+})
+
 
